2080a4024160e390d9190b27846510de3de60793
krapli441 <tlfqj1208@gmail.com>
2023-05-20 16:47:32 +0900

    연습 함수 작성

---
+++ 2023.05.20_tsprac/test.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    // 매개변수 a와 b는 'number'다
+    return a + b;
+}
+console.log(add(2, 2)); // 4
---
+++ 2023.05.20_tsprac/test.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+  // 매개변수 a와 b는 'number'다
+  return a + b;
+}
+
+console.log(add(2, 2)); // 4
